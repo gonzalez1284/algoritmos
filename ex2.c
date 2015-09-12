05c9f870c7ecf02a10b985ce5ae8a43d268202a8
gonzalez1284 <gonzalez1284@gmail.com>
2015-09-12 09:58:38 -0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+  int n1, n2, n3= 0;
+  float m = 0;
+
+  printf("Digite o primeiro valor: ");
+  scanf("%d", &n1);
+  printf("Digite o segundo valor: ");
+  scanf("%d", &n2);
+  printf("Digite o terceiro valor: ");
+  scanf("%d", &n3);
+
+  m = (n1 + n2 + n3) / 3;
+
+  printf("O valor da média é: %f\n", m);
+
+  return(0);
+  
+}
