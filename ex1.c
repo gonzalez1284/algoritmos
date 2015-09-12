97b617e091e69a096c61b10231fa960ed73cdbd8
gonzalez1284 <gonzalez1284@gmail.com>
2015-09-12 09:51:55 -0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+  int a,b = 0;
+
+  printf("Digite o primeiro valor: ");
+  scanf("%d", &a);
+  printf("Digite o segundo valor: ");
+  scanf("%d", &b);
+  printf("O resultado da soma é: %d\n", a+b);
+
+  system("pause");
+  
+  return(0);
+
+
+}
